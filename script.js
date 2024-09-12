7dd3204393d1c392ae5e0524e9d0722a01cc4e6c
LorentzaZweena <ariva02zweena@gmail.com>
2024-09-12 14:31:11 +0700

    front end

---
+++ script.js
@@ -0,0 +1,19 @@
+//repeater
+$(document).ready(function(){
+    $('.repeater').repeater({
+        initEmpty : false,
+        defaultValues : {
+            'text-input' : ''
+        }, 
+        show:function(){
+            $(this).slideDown();
+        }, 
+        hide: function(deleteElement){
+            $(this).slideUp(deleteElement);
+            setTimeout(() => {
+                // generateCV();
+            }, 500);
+        },
+        isFirstItemUndeleteable : true
+    })
+})
