0e280737ac1479104bfa0b43ccd1527e8fe06228
LorentzaZweena <ariva02zweena@gmail.com>
2024-10-30 15:53:01 +0700

    CV builder

--- php/index2.php
+++ php/index2.php
@@ -1,6 +1,10 @@
 <?php
     session_start();
     include 'connection.php';
+    // if (!isset($_SESSION['username'])) {
+    //     header("Location: login.php");
+    //     exit();
+    // }
 ?>
 
 <!DOCTYPE html>
